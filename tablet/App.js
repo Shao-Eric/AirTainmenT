b587f5e9a19814f3f331e825325fae43b194bb01
David Le <David@Davids-MacBook-Air-2.local>
2018-08-10 22:11:27 -0400

    First screen works

--- tablet/App.js
+++ tablet/App.js
@@ -1,7 +1,21 @@
 import React from 'react';
 import Stack from './navigation/stack'
+import firebase from 'firebase'
 
 export default class App extends React.Component {
+  componentWillMount() {
+    let config = {
+      apiKey: "AIzaSyCfgGAENd9pnKaij4hDPKp1ttgswzK4Y1g",
+      authDomain: "airtainment-dba73.firebaseapp.com",
+      databaseURL: "https://airtainment-dba73.firebaseio.com",
+      projectId: "airtainment-dba73",
+      storageBucket: "airtainment-dba73.appspot.com",
+      messagingSenderId: "471409054224"
+    };
+
+    firebase.initializeApp(config);
+  }
+
   render() {
     return (
       <Stack/>
--- tablet/pages/QrScreen.js
+++ tablet/pages/QrScreen.js
@@ -1,11 +1,33 @@
 import React from 'react'
-import {View, Text} from 'react-native'
+import {View, Text, TouchableHighlight} from 'react-native'
+import Qrcode from 'react-native-qrcode'
 
 export default class QrScreen extends React.Component {
+	openMediaLibrary(userid) {
+		this.props.navigation.navigate('MediaScreen',{
+			userid: userid
+		})
+	}
+
   render() {
     return(
       <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
-        <Text>This is a qr code</Text>
+      	<Text style = {{
+      		textAlign: "center",
+      		fontSize: 24,
+      		
+      	}}>Scan with AirghgTainmenT app to begin your personalized experience </Text>
+        <Qrcode style = {{margin: 20}}
+        	value = {"ilovecse"}
+        	size = {300}
+        />
+        <TouchableHighlight onPress={()=>this.openMediaLibrary("userid")}>
+	        <Text style = {{
+	        	textAlign: "center",
+	        	fontSize: 20,
+	        	textDecorationLine: "underline"
+	        }}>Skip</Text>
+        </TouchableHighlight>
       </View>
     )
   }
