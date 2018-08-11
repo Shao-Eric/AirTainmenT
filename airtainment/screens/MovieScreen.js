03108ea294dd41bff389a00fb35f70a8e6d63eaf
Shao Eric Hu <erichu11364@gmail.com>
2018-08-10 21:05:34 -0400

    firebase

--- airtainment/screens/MovieScreen.js
+++ airtainment/screens/MovieScreen.js
@@ -2,6 +2,17 @@ import React from 'react';
 import { Text, View } from 'react-native';
 
 class MovieScreen extends React.Component {
+  state = {
+    loading: false,
+    data: [],
+    page: 1,
+    seed: 1,
+    error: null,
+    refreshing: false
+  };
+
+  componentDidMount() {}
+
   render() {
     return (
       <View>
--- airtainment/screens/WelcomeScreen.js
+++ airtainment/screens/WelcomeScreen.js
@@ -1,29 +1,19 @@
 import React from 'react';
 import { StyleSheet, Text, View, Button } from 'react-native';
-import Header from '../common/Header'
+import Header from '../common/Header';
 import { BarCodeScanner, Permissions } from 'expo';
 import firebase from 'firebase';
 
 class WelcomeScreen extends React.Component {
-
   state = {
     hasCameraPermission: null,
     sessionStarted: false,
     bar: 'Scan a BARcode to begin'
-  }
+  };
 
   async componentWillMount() {
-    const config = {
-      apiKey: "AIzaSyCfgGAENd9pnKaij4hDPKp1ttgswzK4Y1g",
-      authDomain: "airtainment-dba73.firebaseapp.com",
-      databaseURL: "https://airtainment-dba73.firebaseio.com",
-      projectId: "airtainment-dba73",
-      storageBucket: "airtainment-dba73.appspot.com",
-      messagingSenderId: "471409054224"
-    };
-    firebase.initializeApp(config);
     const { status } = await Permissions.askAsync(Permissions.CAMERA);
-    this.setState({hasCameraPermission: status === 'granted'});
+    this.setState({ hasCameraPermission: status === 'granted' });
   }
 
   getBarCode = () => {
@@ -35,17 +25,17 @@ class WelcomeScreen extends React.Component {
       return <Text>No access to camera</Text>;
     } else {
       return (
-          <BarCodeScanner
-            onBarCodeRead={this._handleBarCodeRead}
-            style={StyleSheet.absoluteFill}
-          />
+        <BarCodeScanner
+          onBarCodeRead={this._handleBarCodeRead}
+          style={StyleSheet.absoluteFill}
+        />
       );
     }
-  }
+  };
 
   _handleBarCodeRead = ({ type, data }) => {
     alert(`Bar code with type ${type} and data ${data} has been scanned!`);
-  }
+  };
 
   render() {
     const { hasCameraPermission } = this.state;
@@ -56,9 +46,9 @@ class WelcomeScreen extends React.Component {
       return <Text>No access to camera</Text>;
     } else {
       return (
-        <View style={{ flex: 1, alignItems:'center'}}>
-          <Header  bar={this.state.bar}/>
-          <View style = {{width:400, height:400}}>
+        <View style={{ flex: 1, alignItems: 'center' }}>
+          <Header bar={this.state.bar} />
+          <View style={{ width: 400, height: 400 }}>
             <BarCodeScanner
               onBarCodeRead={this._handleBarCodeRead}
               style={StyleSheet.absoluteFill}
@@ -66,13 +56,10 @@ class WelcomeScreen extends React.Component {
           </View>
           <Text> Scan QR Code </Text>
           <Text> To connect to your screen </Text>
-          <Button title="Manage Library"/>
+          <Button title="Manage Library" />
         </View>
       );
     }
   }
 }
 export default WelcomeScreen;
-
-
-
