a9befb12c538ad5c286bb375ea4cacb8a4e0dfcc
Shao Eric Hu <erichu11364@gmail.com>
2018-08-11 10:12:53 -0400

    update

--- airtainment/screens/MusicScreen.js
+++ airtainment/screens/MusicScreen.js
@@ -1,17 +1,86 @@
 import React from 'react';
-import { Text, View } from 'react-native';
+import { Text, View, FlatList } from 'react-native';
+import firebase from 'firebase';
+import { List, ListItem, SearchBar } from 'react-native-elements';
+import Header from '../common/Header';
+import Card from '../common/Card';
+import CardSection from '../common/CardSection';
 
 class MusicScreen extends React.Component {
+  state = {
+    data: []
+  };
+
+  componentDidMount() {
+    let ref = firebase.database().ref('userid/music');
+    ref.on('value', snapshot => {
+      let result = snapshot.val();
+      console.log(result);
+      this.setState({ data: result });
+    });
+  }
+  renderHeader = () => {
+    return <SearchBar placeholder="Type Here..." lightTheme round />;
+  };
+  renderSeparator = () => {
+    return (
+      <View
+        style={{
+          height: 1,
+          width: '86%',
+          backgroundColor: '#CED0CE',
+          marginLeft: '14%'
+        }}
+      />
+    );
+  };
+
   render() {
     return (
-      <View>
-        <Text>MusicScreen</Text>
-        <Text>MusicScreen</Text>
-        <Text>MusicScreen</Text>
-        <Text>MusicScreen</Text>
-        <Text>MusicScreen</Text>
+      <View style={{ flex: 1 }}>
+        <Header headerText="Music" />
+        <FlatList
+          style={{ flex: 1 }}
+          data={this.state.data}
+          renderItem={({ item }) => (
+            <ListItem
+              roundAvatar
+              title={`${item.title}`}
+              subtitle={item.title}
+              avatar={{ uri: item.image }}
+              containerStyle={{ borderBottomWidth: 0 }}
+            />
+          )}
+          keyExtractor={item => item.link}
+          ItemSeparatorComponent={this.renderSeparator}
+          ListHeaderComponent={this.renderHeader}
+        />
       </View>
     );
   }
 }
+
+const styles = {
+  headerContentStyle: {
+    flexDirection: 'column',
+    justifyContent: 'space-around'
+  },
+  headerTextStyle: {
+    fontSize: 18
+  },
+  thumbnailStyle: {
+    height: 50,
+    width: 50
+  },
+  thumbnailContainerStyle: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginLeft: 10,
+    marginRight: 10
+  },
+  imageStyle: {
+    height: 300,
+    flex: 1
+  }
+};
 export default MusicScreen;
--- airtainment/screens/WelcomeScreen.js
+++ airtainment/screens/WelcomeScreen.js
@@ -1,9 +1,15 @@
 import React from 'react';
-<<<<<<< Updated upstream
-import { Platform, StyleSheet, Text, View, TouchableOpacity, ImageBackground} from 'react-native';
-import Header from '../common/Header'
+import {
+  Platform,
+  StyleSheet,
+  Text,
+  View,
+  TouchableOpacity,
+  ImageBackground
+} from 'react-native';
+import Header from '../common/Header';
 import { BarCodeScanner, Permissions } from 'expo';
-import firebase from 'firebase'
+import firebase from 'firebase';
 
 class WelcomeScreen extends React.Component {
   state = {
@@ -18,11 +24,14 @@ class WelcomeScreen extends React.Component {
   }
 
   buttonPress = () => {
-    this.props.navigation.navigate('main')
-  }
+    this.props.navigation.navigate('main');
+  };
 
   _handleBarCodeRead = ({ type, data }) => {
-    firebase.database().ref(data+"/userid").set("userid")
+    firebase
+      .database()
+      .ref(data + '/userid')
+      .set('userid');
   };
 
   render() {
@@ -34,48 +43,62 @@ class WelcomeScreen extends React.Component {
       return <Text>No access to camera</Text>;
     } else {
       return (
-        <ImageBackground 
-            source={require('../assets/CityDimmed.jpg')}
-            style={{ flex: 1, alignItems:'center'}}>
-            <View style = {{width: '100%'}}>
-             <Header headerText='Tainment' bar={this.state.bar}/>
-            </View>
-            <View style = {{width: 300, height: 300, margin: 60}}>
-              <BarCodeScanner
-                onBarCodeRead={this._handleBarCodeRead}
-                style={StyleSheet.absoluteFill}
-              />
-            </View>
-            {/* <View> */}
-              <Text style={{color:'white',textAlign: 'center', fontWeight: '300',
-                fontFamily: Platform.OS === 'android' ? 'sans-serif-light' : undefined,
-                fontSize: 30}}> Scan QR Code </Text>
-              <Text style={{color:'white', textAlign: 'center', fontWeight: '100', fontSize: 30}}> To connect to your screen </Text>
-              <TouchableOpacity
-                style = {styles.Button}
-                onPress = {() => this.buttonPress()}
-              >
-                <Text style = {{color: 'white', alignSelf:'center', textAlign: 'center'}}>Manage Library</Text>
-              </TouchableOpacity>
-            {/* </View> */}
-          </ImageBackground>
+        <ImageBackground
+          source={require('../assets/CityDimmed.jpg')}
+          style={{ flex: 1, alignItems: 'center' }}
+        >
+          <View style={{ width: '100%' }}>
+            <Header headerText="Tainment" bar={this.state.bar} />
+          </View>
+          <View style={{ width: 300, height: 300, margin: 60 }}>
+            <BarCodeScanner
+              onBarCodeRead={this._handleBarCodeRead}
+              style={StyleSheet.absoluteFill}
+            />
+          </View>
+          {/* <View> */}
+          <Text
+            style={{
+              color: 'white',
+              textAlign: 'center',
+              fontWeight: '300',
+              fontFamily:
+                Platform.OS === 'android' ? 'sans-serif-light' : undefined,
+              fontSize: 30
+            }}
+          >
+            {' '}
+            Scan QR Code{' '}
+          </Text>
+          <Text
+            style={{
+              color: 'white',
+              textAlign: 'center',
+              fontWeight: '100',
+              fontSize: 30
+            }}
+          >
+            {' '}
+            To connect to your screen{' '}
+          </Text>
+          <TouchableOpacity
+            style={styles.Button}
+            onPress={() => this.buttonPress()}
+          >
+            <Text
+              style={{
+                color: 'white',
+                alignSelf: 'center',
+                textAlign: 'center'
+              }}
+            >
+              Manage Library
+            </Text>
+          </TouchableOpacity>
+          {/* </View> */}
+        </ImageBackground>
       );
     }
-=======
-import { Text, View } from 'react-native';
-
-class WelcomeScreen extends React.Component {
-  render() {
-    return (
-      <View>
-        <Text>WelcomeScreen</Text>
-        <Text>WelcomeScreen</Text>
-        <Text>WelcomeScreen</Text>
-        <Text>WelcomeScreen</Text>
-        <Text>WelcomeScreen</Text>
-      </View>
-    );
->>>>>>> Stashed changes
   }
 }
 
@@ -85,9 +108,9 @@ const styles = StyleSheet.create({
     width: 150,
     height: 50,
     borderRadius: 40,
-    display:'flex',
-    justifyContent:'center',
+    display: 'flex',
+    justifyContent: 'center',
     backgroundColor: '#085ff7'
   }
-})
+});
 export default WelcomeScreen;
