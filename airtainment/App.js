75f2735c2d05e9d00022a1a468d99ed0291442d8
Shao Eric Hu <erichu11364@gmail.com>
2018-08-10 20:19:25 -0400

    screens rdy

--- airtainment/App.js
+++ airtainment/App.js
@@ -1,15 +1,35 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import {
+  createBottomTabNavigator,
+  createStackNavigator
+} from 'react-navigation';
+import WelcomeScreen from './screens/WelcomeScreen';
+import MovieScreen from './screens/MovieScreen';
+import MusicScreen from './screens/MusicScreen';
+import TVScreen from './screens/TVScreen';
+
+const MainNavigator = createBottomTabNavigator(
+  {
+    welcome: { screen: WelcomeScreen },
+    main: {
+      screen: createBottomTabNavigator({
+        movie: { screen: MovieScreen },
+        tv: { screen: TVScreen },
+        music: { screen: MusicScreen }
+      })
+    }
+  }
+  // {
+  //   navigationOptions: {
+  //     tabBarVisible: false
+  //   }
+  // }
+);
 
 export default class App extends React.Component {
   render() {
-    return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
-      </View>
-    );
+    return <MainNavigator />;
   }
 }
 
@@ -18,6 +38,6 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
-  },
+    justifyContent: 'center'
+  }
 });
