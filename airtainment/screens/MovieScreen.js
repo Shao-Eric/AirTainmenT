2d8f41c27a051d9797873a2e7ea7d193c013eb25
Shao Eric Hu <erichu11364@gmail.com>
2018-08-11 12:07:27 -0400

    icon

--- airtainment/screens/MovieScreen.js
+++ airtainment/screens/MovieScreen.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Text, View, FlatList } from 'react-native';
 import firebase from 'firebase';
 import { List, ListItem, SearchBar } from 'react-native-elements';
+import { Entypo } from '@expo/vector-icons';
 import Header from '../common/Header';
 import Card from '../common/Card';
 import CardSection from '../common/CardSection';
@@ -10,6 +11,10 @@ class MovieScreen extends React.Component {
   state = {
     data: []
   };
+
+  static navigationOptions = ({ navigation }) => ({
+    tabBarIcon: () => <Entypo name="tv" size={30} color="#1b3039" />
+  });
 
   componentDidMount() {
     let ref = firebase.database().ref('userid/music');
